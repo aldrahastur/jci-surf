5efc2e046bfc597aecfbf8c53a5c267165c82c14
Laravel Daily <info@laraveldaily.com>
2021-08-01 09:37:59 +0000

    QuickAdminPanel automatic commit

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -2,10 +2,42 @@
 
 namespace App\Http\Controllers\Admin;
 
+use LaravelDaily\LaravelCharts\Classes\LaravelChart;
+
 class HomeController
 {
     public function index()
     {
-        return view('home');
+        $settings1 = [
+            'chart_title'           => 'New Listings',
+            'chart_type'            => 'latest_entries',
+            'report_type'           => 'group_by_date',
+            'model'                 => 'App\Models\Listing',
+            'group_by_field'        => 'created_at',
+            'group_by_period'       => 'day',
+            'aggregate_function'    => 'count',
+            'filter_field'          => 'created_at',
+            'group_by_field_format' => 'Y-m-d H:i:s',
+            'column_class'          => 'col-md-4',
+            'entries_number'        => '10',
+            'fields'                => [
+                'name'       => '',
+                'created_at' => '',
+            ],
+            'translation_key' => 'listing',
+        ];
+
+        $settings1['data'] = [];
+        if (class_exists($settings1['model'])) {
+            $settings1['data'] = $settings1['model']::latest()
+                ->take($settings1['entries_number'])
+                ->get();
+        }
+
+        if (!array_key_exists('fields', $settings1)) {
+            $settings1['fields'] = [];
+        }
+
+        return view('home', compact('settings1'));
     }
 }
